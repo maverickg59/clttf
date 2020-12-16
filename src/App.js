393893282039c0d199536ddd53557f2cc939bdd4
Christopher White <christopher.evan.white88@gmail.com>
2020-12-16 00:16:36 -0700

    removed switch import to prevent netlify failing due to warning...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { iconLibrary } from 'maverick-toolkit-react'
 import { icons } from './lib'
 import { Header, Footer } from './components'
