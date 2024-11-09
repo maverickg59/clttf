b9fd526b7dd09b341bf16414b498e5a3df9f095e
Chris White <chris@chriswhite.rocks>
2024-11-08 16:08:18 -0800

    reverts node version changes as not necessary

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -170,9 +170,7 @@ export function Contact() {
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-zinc-600 sm:text-sm/6"
                   />
                   <span>
-                    {formError && formError.phoneNumber
-                      ? formError.phoneNumber
-                      : ''}
+                    {formError?.phoneNumber ? formError.phoneNumber : ''}
                   </span>
                 </div>
               </div>
