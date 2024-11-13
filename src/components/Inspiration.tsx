ffc4974d381cf336bc72aeaa0414e73f88b9a88e
Chris White <chris@chriswhite.rocks>
2024-11-13 11:20:18 -0800

    correct build failure

--- src/components/Inspiration.tsx
+++ src/components/Inspiration.tsx
@@ -20,7 +20,7 @@ export function Inspiration() {
           />
           <div className="mx-auto flex w-5/6 flex-col justify-center text-center sm:w-1/2 sm:text-left">
             <h3 className="mt-6 text-lg/8 font-semibold text-gray-900">
-              Mike "Coach" Lawson
+              Mike &quot;Coach&quot; Lawson
             </h3>
             <p className="text-base/7 text-gray-600">8/26/1983 - 9/22/2020</p>
             <p className="mt-4 text-base/7 text-gray-600">
