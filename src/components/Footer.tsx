16069760a96002eadc46c47873c56dbb88b90521
Chris White <chris@chriswhite.rocks>
2024-11-16 15:09:42 -0800

    modifies fb links to use mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,7 @@ export function Footer() {
       <Container className="flex flex-col justify-center gap-2 lg:items-end lg:text-right">
         <p className="flex items-center justify-center fill-zinc-500 text-zinc-500 hover:fill-zinc-900 hover:text-zinc-900">
           <Link
-            href="https://facebook.com/102960111692089"
+            href="https://m.facebook.com/102960111692089"
             className="text-sm focus:outline-zinc-500 md:mt-0"
             target="_blank"
             rel="noopener noreferrer"
--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -109,7 +109,7 @@ export function Newsletter() {
               </p>
               <div className="mt-3 flex items-center fill-zinc-900 align-middle text-lg text-zinc-900 hover:fill-zinc-500 hover:text-zinc-600">
                 <Link
-                  href="https://facebook.com/102960111692089"
+                  href="https://m.facebook.com/102960111692089"
                   className="text-md focus:outline-zinc-900 md:mt-0"
                   target="_blank"
                   rel="noopener noreferrer"
